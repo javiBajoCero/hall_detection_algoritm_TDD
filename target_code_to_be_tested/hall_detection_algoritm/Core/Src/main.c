6d7fab8de952c7be59d73470cdf9b21a1b2726b7
Javier <javimusama@gmail.com>
2024-01-30 09:27:35 +0100

    trying to makefile with a stcubeide session

--- target_code_to_be_tested/hall_detection_algoritm/Core/Src/main.c
+++ target_code_to_be_tested/hall_detection_algoritm/Core/Src/main.c
@@ -97,8 +97,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_Delay(1000);
-    HAL_GPIO_TogglePin (LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(100);
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
