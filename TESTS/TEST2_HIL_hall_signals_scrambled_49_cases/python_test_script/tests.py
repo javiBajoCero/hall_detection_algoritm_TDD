5865df043d08462233865dec4bfaa0e6f02a6559
Javier <javimusama@gmail.com>
2024-02-02 12:25:39 +0100

    Update tests.py

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -13,18 +13,27 @@ import serial
 serTester = serial.Serial('/dev/ttyACM0', 115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
 serTarget = serial.Serial('/dev/ttyACM1', 115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
 time.sleep(0.2)
-print "The ports are opened"
+print("The ports are opened")
+
+serTester.write(b'emulation/n/r')     # write a string
+line = serTester.readlines()   # read a '\n' terminated line
+print(line)
 
 serTester.write(b'ABC/n/r')     # write a string
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
+
+serTester.write(b'reset target/n/r')     # write a string
+line = serTester.readlines()   # read a '\n' terminated line
+print(line)
+
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
 
 serTester.close()
 serTarget.close()
 time.sleep(0.2)
-print "The ports are closed"
+print("The ports are closed")
 
 
 
