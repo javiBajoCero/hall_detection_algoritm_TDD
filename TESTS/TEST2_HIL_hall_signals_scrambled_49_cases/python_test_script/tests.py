d1bd6b1ac7d347f65a1dd5b02c749971eb6f89a3
Javier <javimusama@gmail.com>
2024-02-02 12:37:56 +0100

    Update tests.py

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -10,8 +10,12 @@
 
 
 import serial
+import time
+
+
 serTester = serial.Serial('/dev/ttyACM0', 115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
 serTarget = serial.Serial('/dev/ttyACM1', 115200, bytesize=8, parity='N', stopbits=1, timeout=5, xonxoff=0, rtscts=0)
+time.sleep(0.300)
 print("The ports are opened")
 
 serTester.write(b'emulation/n/r')     # write a string
@@ -19,26 +23,31 @@ line = serTester.readlines()   # read a '\n' terminated line
 print(line)
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
+time.sleep(0.200)
 
 serTester.write(b'ABC/n/r')     # write a string
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
+time.sleep(0.200)
 
 serTester.write(b'reset target/n/r')     # write a string
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
+time.sleep(0.200)
 
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
 line = serTester.readlines()   # read a '\n' terminated line
 print(line)
+time.sleep(0.200)
 
 serTester.close()
 serTarget.close()
+time.sleep(0.300)
 print("The ports are closed")
 
 
