f27db4af4cfee889ad91022f09652591654c0066
Javier <javimusama@gmail.com>
2024-02-02 14:05:39 +0100

    Update tests.py

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -13,8 +13,8 @@
 import serial
 import time
 
-serTarget = serial.Serial('/dev/ttyACM1', 115200, bytesize=8, parity='N', stopbits=1, timeout=1, xonxoff=0, rtscts=0);
-serTester = serial.Serial('/dev/ttyACM2', 115200, bytesize=8, parity='N', stopbits=1, timeout=1, xonxoff=0, rtscts=0);
+serTarget = serial.Serial('/dev/ttyACM2', 115200, bytesize=8, parity='N', stopbits=1, timeout=2, xonxoff=0, rtscts=0);
+serTester = serial.Serial('/dev/ttyACM1', 115200, bytesize=8, parity='N', stopbits=1, timeout=2, xonxoff=0, rtscts=0);
 time.sleep(0.300);
 print("The ports are opened");
 
