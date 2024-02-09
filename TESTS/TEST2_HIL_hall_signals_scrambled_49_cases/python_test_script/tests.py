c8c46d05201f817af0a811596a28d9c64b1028b2
javi ubuntu pc <javimusama@gmail.com>
2024-02-09 11:53:25 +0100

    test scrit improvement

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -1,216 +1,160 @@
-
-# previously ran on the console
-# sudo apt update -y
-# sudo apt install python3-venv python3-pip -y
-
-# python -m pip install pyserial
-
-#/dev/ttyACM0
-#/dev/ttyAMA0
-
-#python -m serial.tools.list_ports
-
-#import serial
-#import time
-#
-#serTarget = serial.Serial('/dev/ttyACM2', 115200, bytesize=8, parity='N', stopbits=1, timeout=2, xonxoff=0, rtscts=0);
-#serTester = serial.Serial('/dev/ttyACM1', 115200, bytesize=8, parity='N', stopbits=1, timeout=2, xonxoff=0, rtscts=0);
-#time.sleep(0.300);
-#print("The ports are opened");
-#
-#
-#serTester.write(b'emulation/n/r') ;    # write a string
-#print(serTester.read(size=50));
-#
-#
-#serTester.close();
-#serTarget.close();
-#time.sleep(0.300);
-#print("The ports are closed");
-#
-
 import serial
 import serial.tools.list_ports
 import time
+import threading
+    
+waittime=0.25;
+stopthreads=True;
+serial_tester=0;
+serial_target=0;
 
-waittime=0.5;
-sent_index=0;
+receivedMessages=[]
+messages = [
+            "ABC\n\r",
+            "!ABC\n\r",
+            "!A!BC\n\r",
+            "!A!B!C\n\r",
+            "!AB!C\n\r",
+            "AB!C\n\r",
+            "A!B!C\n\r",
+            "A!BC\n\r",
+            "BCA\n\r",
+            "!BCA\n\r",
+            "!B!CA\n\r",
+            "!B!C!A\n\r",
+            "!BC!A\n\r",
+            "BC!A\n\r",
+            "B!C!A\n\r",
+            "B!CA\n\r",
+            "CAB\n\r",
+            "!CAB\n\r",
+            "!C!AB\n\r",
+            "!C!A!B\n\r",
+            "!CA!B\n\r",
+            "CA!B\n\r",
+            "C!A!B\n\r",
+            "C!AB\n\r",
+            "ACB\n\r",
+            "!ACB\n\r",
+            "!A!CB\n\r",
+            "!A!C!B\n\r",
+            "!AC!B\n\r",
+            "AC!B\n\r",
+            "A!C!B\n\r",
+            "A!CB\n\r",
+            "BAC\n\r",
+            "!BAC\n\r",
+            "!B!AC\n\r",
+            "!B!A!C\n\r",
+            "!BA!C\n\r",
+            "BA!C\n\r",
+            "B!A!C\n\r",
+            "B!AC\n\r",
+            "CBA\n\r",
+            "!CBA\n\r",
+            "!C!BA\n\r",
+            "!C!B!A\n\r",
+            "!CB!A\n\r",
+            "CB!A\n\r",
+            "C!B!A\n\r",
+            "C!BA\n\r",
+        ]
 
-def list_available_ports():
-    ports = serial.tools.list_ports.comports()
-    print("Available serial ports:")
-    for port in ports:
-        print(f"- {port.device}")
 
-def send_messages(ser, messages):
-    for message in messages:
-        ser.write(message.encode())
-        print(f'Sent message: {message}')
-        time.sleep(waittime)  # Add a delay to allow the device to process the message
-
-def receive_messages(ser):
-    if ser.in_waiting > 0:
-        received_data = ser.readline().decode().strip()
-        if received_data != "":
-            print(f"Received: {received_data}")
-
-def main():
-    # List available serial ports
-    list_available_ports()
-
-    # Define the serial connection parameters
+def find_out_tester_and_open_ports():
+    global serial_tester;
+    global serial_target;
+        # Define the serial connection parameters
     baud_rate = 115200
     byte_size = 8
     parity = 'N'
     stop_bits = 1
-    timeout = 10  # Increased timeout
+    timeout = waittime
     xonxoff = 0
     rtscts = 0
+    
+    ports = serial.tools.list_ports.comports()
+    print("Available serial ports:")
+    for port in ports:
+        if(port.pid==14155):
+            print(f"- found target board in : {port.device}  with pid: {port.pid}")
+            serial_target = serial.Serial(port=port.device,baudrate=baud_rate,bytesize=byte_size,parity=parity,stopbits=stop_bits,timeout=timeout,xonxoff=xonxoff,rtscts=rtscts)
+            serial_target.flush();
+        elif (port.pid==14164):
+            print(f"- found tester board in : {port.device}  with pid: {port.pid}")
+            serial_tester = serial.Serial(port=port.device,baudrate=baud_rate,bytesize=byte_size,parity=parity,stopbits=stop_bits,timeout=timeout,xonxoff=xonxoff,rtscts=rtscts)
+            serial_tester.flush();
+            
+def send_messages_tester(ser, messages):
+    resetmessage="reset target\n\r";
+    emulationmessage="emulation\n\r";
+    serial_tester.write(emulationmessage.encode());
+    time.sleep(waittime)  # Add a delay to allow the device to process the message
 
-    try:
-        ser = None
-        for port_info in serial.tools.list_ports.comports():
-            try:
-                # Try opening the serial connection on each available port
-                ser = serial.Serial(
-                    port='/dev/ttyACM1',
-                    baudrate=baud_rate,
-                    bytesize=byte_size,
-                    parity=parity,
-                    stopbits=stop_bits,
-                    timeout=timeout,
-                    xonxoff=xonxoff,
-                    rtscts=rtscts
-                )
-                print(f"Serial connection opened on {port_info.device} at {baud_rate} baud.")
-                break  # Stop iterating if a valid connection is established
-
-            except serial.SerialException as e:
-                print(f"Error opening port {port_info.device}: {e}")
-
-        if ser is not None:
-            # Define the messages to be sent
-            messages = [
-              "emulation\n\r",
-                "ABC\n\r",
-                "reset target\n\r",
-                "!ABC\n\r",
-                "reset target\n\r",
-                "!A!BC\n\r",
-                "reset target\n\r",
-                "!A!B!C\n\r",
-                "reset target\n\r",
-                "!AB!C\n\r",
-                "reset target\n\r",
-                "AB!C\n\r",
-                "reset target\n\r",
-                "A!B!C\n\r",
-                "reset target\n\r",
-                "A!BC\n\r",
-                "reset target\n\r",
-                "BCA\n\r",
-                "reset target\n\r",
-                "!BCA\n\r",
-                "reset target\n\r",
-                "!B!CA\n\r",
-                "reset target\n\r",
-                "!B!C!A\n\r",
-                "reset target\n\r",
-                "!BC!A\n\r",
-                "reset target\n\r",
-                "BC!A\n\r",
-                "reset target\n\r",
-                "B!C!A\n\r",
-                "reset target\n\r",
-                "B!CA\n\r",
-                "reset target\n\r",
-                "CAB\n\r",
-                "reset target\n\r",
-                "!CAB\n\r",
-                "reset target\n\r",
-                "!C!AB\n\r",
-                "reset target\n\r",
-                "!C!A!B\n\r",
-                "reset target\n\r",
-                "!CA!B\n\r",
-                "reset target\n\r",
-                "CA!B\n\r",
-                "reset target\n\r",
-                "C!A!B\n\r",
-                "reset target\n\r",
-                "C!AB\n\r",
-                "reset target\n\r",
-                "ACB\n\r",
-                "reset target\n\r",
-                "!ACB\n\r",
-                "reset target\n\r",
-                "!A!CB\n\r",
-                "reset target\n\r",
-                "!A!C!B\n\r",
-                "reset target\n\r",
-                "!AC!B\n\r",
-                "reset target\n\r",
-                "AC!B\n\r",
-                "reset target\n\r",
-                "A!C!B\n\r",
-                "reset target\n\r",
-                "A!CB\n\r",
-                "reset target\n\r",
-                "BAC\n\r",
-                "reset target\n\r",
-                "!BAC\n\r",
-                "reset target\n\r",
-                "!B!AC\n\r",
-                "reset target\n\r",
-                "!B!A!C\n\r",
-                "reset target\n\r",
-                "!BA!C\n\r",
-                "reset target\n\r",
-                "BA!C\n\r",
-                "reset target\n\r",
-                "B!A!C\n\r",
-                "reset target\n\r",
-                "B!AC\n\r",
-                "reset target\n\r",
-                "CBA\n\r",
-                "reset target\n\r",
-                "!CBA\n\r",
-                "reset target\n\r",
-                "!C!BA\n\r",
-                "reset target\n\r",
-                "!C!B!A\n\r",
-                "reset target\n\r",
-                "!CB!A\n\r",
-                "reset target\n\r",
-                "CB!A\n\r",
-                "reset target\n\r",
-                "C!B!A\n\r",
-                "reset target\n\r",
-                "C!BA\n\r",
-                "reset target\n\r",
-            ]
-
-            # Start a thread or a separate process to receive incoming messages
-            import threading
-            receive_thread = threading.Thread(target=receive_messages, args=(ser),daemon=True) 
-            receive_thread.start();
-            # Send messages
-            send_messages(ser, messages)
-
-            # Wait for the receive thread to finish (or handle it differently based on your needs)
-            receive_thread.join()
-
-            # Close the serial connection
-            ser.close()
-            print("Serial connection closed.")
-
-    except serial.SerialException as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    main()
+    for message in messages:
+        serial_tester.write(message.encode())
+        if message.find('reset')!=0:
+            print(f'Sent message to tester: {message}')
+        time.sleep(waittime/2)  # Add a delay to allow the device to process the message
+        serial_tester.write(resetmessage.encode());
+        time.sleep(waittime)  # Add a delay to allow the device to process the message
 
 
+def receive_messages_tester(ser,messages):
+    global stopthreads
+    while stopthreads==True:
+        if ser.in_waiting:
+            received_data = ser.readline().decode()
+            if received_data.find('\r')!=0:
+                print(f"received from tester: {received_data}")
+
+def receive_messages_target(ser,messages):
+    global stopthreads
+    while stopthreads==True:
+        if ser.in_waiting:
+            received_data = ser.readline().decode().replace(' ','')
+            if received_data.find('\r')!=0:
+                print(f"Received from target: {received_data}")
+                receivedMessages.append(received_data);
+
+def main():
+    global messages;
+    # List and open available serial ports
+    find_out_tester_and_open_ports()
 
 
+    # Start a thread or a separate process to receive incoming messages
+    receive_thread = threading.Thread(target=receive_messages_tester, args=(serial_tester,messages),daemon=True)
 
+    #receive_thread.start();
+    
+    receive_thread_test = threading.Thread(target=receive_messages_target, args=(serial_target,messages),daemon=True)
+
+    receive_thread_test.start();
+    
+    send_thread = threading.Thread(target=send_messages_tester, args=(serial_tester,messages),daemon=True) 
+    send_thread.start();
+
+    # Wait for the receive thread to finish (or handle it differently based on your needs)
+    send_thread.join()
+    global stopthreads
+    stopthreads=False;
+    #receive_thread.join();
+    receive_thread_test.join();
+    # Close the serial connection
+    serial_tester.close()
+    print("Serial connection closed.")
+
+    for i in range(len(messages)):
+        messages[i]=messages[i].replace('\r','');
+    
+    if receivedMessages == messages:
+        print("all test cases passed");
+    else:
+        print("not all test cases passed");
+        for i in range(len(receivedMessages)):
+            if messages[i].replace('\r','')!=receivedMessages[i]:
+                print(f"error in test case number {i}, it was supposed to be:{messages[i]} instead of: {receivedMessages[i]}")
+            
+
+main()
