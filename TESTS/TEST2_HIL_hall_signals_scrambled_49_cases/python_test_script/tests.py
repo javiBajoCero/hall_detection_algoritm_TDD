56869470f96f434254ca38adc8caeae126e6f27f
Javier <javimusama@gmail.com>
2024-02-09 15:49:18 +0100

    Update tests.py

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -96,7 +96,7 @@ def send_messages_tester(ser, messages):
     for message in messages:
         serial_tester.write(message.encode())
         if message.find('reset')!=0:
-            print(f'test case {i} ,messsage : {message}')
+            print(f'test case {send_index} ,messsage : {message}')
             send_index=send_index+1;
         time.sleep(waittime/2)  # Add a delay to allow the device to process the message
         serial_tester.write(resetmessage.encode());
