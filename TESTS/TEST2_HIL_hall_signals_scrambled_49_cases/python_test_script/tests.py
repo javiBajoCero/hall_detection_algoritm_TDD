b152149ed10aa731cd3b57c9de47cd8bb4ec102b
javi ubuntu pc <javimusama@gmail.com>
2024-02-09 12:18:53 +0100

    improving readability for the python script

--- TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
+++ TESTS/TEST2_HIL_hall_signals_scrambled_49_cases/python_test_script/tests.py
@@ -95,7 +95,7 @@ def send_messages_tester(ser, messages):
     for message in messages:
         serial_tester.write(message.encode())
         if message.find('reset')!=0:
-            print(f'Sent message to tester: {message}')
+            print(f'Sent message tester: {message}')
         time.sleep(waittime/2)  # Add a delay to allow the device to process the message
         serial_tester.write(resetmessage.encode());
         time.sleep(waittime)  # Add a delay to allow the device to process the message
